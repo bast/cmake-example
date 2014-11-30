63a2762eac2a043d2d5e2c04d6482eeb780571d7
Radovan Bast <rbast@users.noreply.github.com>
2014-11-30 17:54:47 +0100

    outsource make install and cpack code

---
+++ cmake/ConfigInstall.cmake
@@ -0,0 +1,16 @@
+# install binary
+install(TARGETS main.x DESTINATION cmake-example/bin)
+# install libs
+install(TARGETS mylib_shared DESTINATION cmake-example/lib)
+install(TARGETS mylib_static DESTINATION cmake-example/lib)
+# install headers
+install(FILES ${PROJECT_SOURCE_DIR}/src/mylib.h DESTINATION cmake-example/include)
+
+# cpack
+include(InstallRequiredSystemLibraries)
+set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE")
+set(CPACK_PACKAGE_VERSION_MAJOR "${VERSION_MAJOR}")
+set(CPACK_PACKAGE_VERSION_MINOR "${VERSION_MINOR}")
+set(CPACK_PACKAGE_VERSION_PATCH "${VERSION_PATCH}")
+set(CPACK_PACKAGE_CONTACT "Kilgore Trout")
+include(CPack)
