a102197c8d24595d589a31de63967b89481775df
Radovan Bast <rbast@users.noreply.github.com>
2014-11-30 18:49:04 +0100

    get git hash into the output

---
+++ cmake/ConfigGitRevision.cmake
@@ -0,0 +1,14 @@
+find_package(Git)
+if(GIT_FOUND)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-list --max-count=1 HEAD
+        OUTPUT_VARIABLE GIT_REVISION
+        ERROR_QUIET
+        )
+    if(NOT ${GIT_REVISION} STREQUAL "")
+        string(STRIP ${GIT_REVISION} GIT_REVISION)
+    endif()
+    message(STATUS "Current git revision is ${GIT_REVISION}")
+else()
+    set(GIT_REVISION "unknown")
+endif()
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char** argv)
 {
     printf("program version: %i.%i.%i\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+    printf("git hash: %s\n", GIT_REVISION);
 
     do_something();
     do_something_else(); // generated
