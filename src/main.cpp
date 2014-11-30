b55849b5aa47a05f2f5b901a0b0f7ddc01438105
Radovan Bast <rbast@users.noreply.github.com>
2014-11-30 17:41:01 +0100

    configure version

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
+#include <cstdio>
+
 #include "mylib.h"
+#include "config.h"
 
 int main(int argc, char** argv)
 {
+    printf("program version: %i.%i.%i\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+
     do_something();
     return 0;
 }
