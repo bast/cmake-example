28be61982917080b24b2e49c4019e4f2fee131a4
Radovan Bast <rbast@users.noreply.github.com>
2014-11-30 15:54:04 +0100

    fix drop site

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
 set(CTEST_PROJECT_NAME       "cmake-example")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 set(CTEST_DROP_METHOD        "http")
-set(CTEST_DROP_SITE          "http://my.cdash.org")
+set(CTEST_DROP_SITE          "my.cdash.org")
 set(CTEST_DROP_LOCATION      "/submit.php?project=cmake-example")
 set(CTEST_DROP_SITE_CDASH    TRUE)
 set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 200)
