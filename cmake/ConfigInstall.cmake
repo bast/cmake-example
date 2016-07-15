ed1df4288b1f242b564da3171e66967c20d05d81
Radovan Bast <bast@users.noreply.github.com>
2016-07-15 18:33:42 +0200

    more air between lines

--- cmake/ConfigInstall.cmake
+++ cmake/ConfigInstall.cmake
@@ -1,8 +1,10 @@
 # install binary
 install(TARGETS main.x DESTINATION cmake-example/bin)
+
 # install libs
 install(TARGETS mylib_shared DESTINATION cmake-example/lib)
 install(TARGETS mylib_static DESTINATION cmake-example/lib)
+
 # install headers
 install(FILES ${PROJECT_SOURCE_DIR}/src/mylib.h DESTINATION cmake-example/include)
 
