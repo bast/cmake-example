0009018c99e47e30a556eff5d205634e4fcd3a56
Radovan Bast <bast@users.noreply.github.com>
2018-03-13 21:34:00 +0100

    split off packaging as own module

--- cmake/install_target.cmake
+++ cmake/install_target.cmake
@@ -7,12 +7,3 @@ install(TARGETS mylib_static DESTINATION cmake-example/lib)
 
 # install headers
 install(FILES ${PROJECT_SOURCE_DIR}/src/mylib.h DESTINATION cmake-example/include)
-
-# cpack
-include(InstallRequiredSystemLibraries)
-set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE")
-set(CPACK_PACKAGE_VERSION_MAJOR "${VERSION_MAJOR}")
-set(CPACK_PACKAGE_VERSION_MINOR "${VERSION_MINOR}")
-set(CPACK_PACKAGE_VERSION_PATCH "${VERSION_PATCH}")
-set(CPACK_PACKAGE_CONTACT "Kilgore Trout")
-include(CPack)
